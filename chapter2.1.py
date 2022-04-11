1f88707a37ab084463c81f3f87b453278bf8345c
Mriganm102 <72470268+Mriganm102@users.noreply.github.com>
2022-04-11 18:50:16 -0400

    HW_4/11/22

---
+++ chapter2.1.py
@@ -0,0 +1,30 @@
+def power_set(set1):
+    set1 = list(set1)
+    cardinality = len(set1)
+    binary = []
+    for i in range(2**cardinality):
+        b = bin(i)[2:]
+        l = len(b)
+        b = str(0) * (cardinality - l) + b
+        binary.append(b)
+    list1 = []
+    list2 = []
+    powerset = []
+    for x in binary:
+        for y in x:
+            list1.append(y)
+    binary1 = [list1[x:x + cardinality] for x in range(0, len(list1), cardinality)]
+    for x in binary1:
+        for y in range(0, cardinality):
+            if x[y] == "1":
+                list2.append(set1[y])
+        powerset.append(list2)
+        list2 = []
+    print(powerset)
+    return powerset
+
+
+
+
+
+power_set({1,2,3,4,5})
